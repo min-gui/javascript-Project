d128427d86f627e662af68ec7d493e7c5cc46302
min-gui <msj349911@gmail.com>
2023-06-10 14:51:02 +0900

    movie app

---
+++ movie_app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// import logo from './logo.svg';
+// import './App.css';
+
+const Movie = () => {
+  return <h1>I like potato</h1>;
+}
+
+function App() {
+  // return <div className="App"/>
+ 
+  return (  
+    <div>
+      <h1>hello</h1>
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />ß
+    </div>
+  );
+  
+  
+  
+}
+
+export default App;
