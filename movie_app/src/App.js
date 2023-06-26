4b80b1377d99a3de2a930218ad9470d861b7b012
min-gui <msj349911@gmail.com>
2023-06-26 23:15:45 +0900

    state 만들어 보기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,60 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-// import logo from './logo.svg';
-// import './App.css';
-
-const Food = ({name, picture, rating}) => {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-  )
-}
-
-const foodILike = [
-{
-  id : 1,
-  name: "kimchi",
-  image: "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg",
-  rating: 5
-},
-{
-  id : 2,
-  name: "samgyeopsal",
-  image: "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg",
-  rating: 4.9
-},
-{
-  id : 3,
-  name: "samgyeopsal",
-  image: "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg",
-  rating: 3
-}
-];
 
 
-function App() {
-  // return <div className="App"/>
-  return (  
-    <div>
-      {foodILike.map(dish =>
-          <Food key={dish.id} name={dish.name} picture={dish.image} 
-          rating={dish.rating}/>
-        )}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-  Food.propTypes = {
-    name : PropTypes.string.isRequired,
-    picture : PropTypes.string.isRequired,
-    rating : PropTypes.number.isRequired
+  render() {
+    return <h1>The number is: {this.state.count}</h1>
   }
-  
-  
-  
+
 }
 
 export default App;
