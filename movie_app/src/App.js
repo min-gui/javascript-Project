fccf3a607bf66b64a46cbff9765ba50fd0e96750
min-gui <msj349911@gmail.com>
2023-07-28 01:07:49 +0900

    영화데이터 가져오기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    console.log(movies);
   };
 
   //컴포넌트가 만들어지고 render가 된 후에 실행
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false})
-    }, 1000)
+    // setTimeout(() => {
+    //   this.setState({ isLoading: false})
+    // }, 1000)
+    this.getMovies();
   }
 
   render() {
