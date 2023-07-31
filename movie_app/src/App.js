3b00f27813edf3a3109a8e3bf8d584893e61df04
min-gui <msj349911@gmail.com>
2023-08-01 01:12:22 +0900

    state update 하기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
       },
     } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
     console.log(movies)
+    this.setState({movies, isLoading: false});
     // const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
     // console.log(movies.data.data.movies);
   };
