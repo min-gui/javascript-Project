0706d5d3e8cf6869ce0555499c1179f6ca1ccda1
min-gui <msj349911@gmail.com>
2023-08-13 17:05:46 +0900

    prototype 추가

---
+++ movie_app/src/Movie.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie(){
+    return <h1></h1>;
+}
+
+Movie.prototype = {
+    id : PropTypes.number.isRequired,
+    year : PropTypes.number.isRequired,
+    title : PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired,
+};
+
+export default Movie;
