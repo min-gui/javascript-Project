573b8d58021752055c47513859bdd36c3852cee2
min-gui <msj349911@gmail.com>
2023-09-08 01:15:59 +0900

    라우터 적용 완료

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './components/Movie';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-    return <Home />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
